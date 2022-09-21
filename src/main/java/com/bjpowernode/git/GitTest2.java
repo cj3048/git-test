0362bf390aee85099ec9fd117c8a7b6b9326353c
chenjuan <chenjuan@example.com>
2022-09-21 10:43:20 +0800

    2 commit2

--- src/main/java/com/bjpowernode/git/GitTest2.java
+++ src/main/java/com/bjpowernode/git/GitTest2.java
@@ -3,6 +3,7 @@ package com.bjpowernode.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hello,git!!!");
+        System.out.println("hello,git22!!!");
 
     }
 }
