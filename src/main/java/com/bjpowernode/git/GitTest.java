be49c0617460774c190ba2e21ab929503080dd12
chenjuan <chenjuan@example.com>
2022-09-21 10:20:46 +0800

    Initial commit

---
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!!!");
+        System.out.println("hello,git22!!!");
+        System.out.println("hello,git3!!!");
+        System.out.println("hello,git4!!!");
+        System.out.println("master,test!!!");
+        System.out.println("hot-fix,test!!!");
+        System.out.println("hot-fix2,test!!");
+    }
+}
